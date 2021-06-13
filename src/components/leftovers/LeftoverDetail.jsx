765edf91004fff58899491f250ea51e6311dfa04
rsaleh <raihan.saleh@gmail.com>
2021-06-13 05:00:28 -0400

    Myfridge functionality WIP

--- src/components/leftovers/LeftoverDetail.jsx
+++ src/components/leftovers/LeftoverDetail.jsx
@@ -7,49 +7,6 @@ import DeleteModal from './DeleteModal'
 import ClaimModal from './ClaimModal'
 import Expiration from './Expiration'
 
-
-/* ==============================================
-            Claim Modal Component
-=============================================== */
-// function ClaimModal({ setModal, leftover }) {
-//   const { api_url } = useContext(LeftoverContext);
-
-//   const claimRequest = {
-//     url: `${api_url}/orders`,
-//     config: {
-//       headers: {
-//         Authorization: `JWT ${localStorage.getItem("token")}`,
-//         "Content-Type": "application/json",
-//       },
-//     },
-//     reqBody: { is_available: "false" },
-//   };
-
-//   function ClaimLeftover(claimRequest) {
-//     // axios post request to orders
-//     // axios
-//     //   .post(claimRequest.url, claimRequest.reqBody, claimRequest.config)
-//     //   .then((res) => {
-//     //     console.log(res);
-//     //   })
-//     //   .then(setModal())
-//     //   .catch((error) => console.error);
-//   }
-//   return (
-//     <div>
-//       <h2>Lets Claim this leftover</h2>
-//       <button onClick={() => ClaimLeftover(claimRequest)}>
-//         Claim Leftover
-//       </button>
-//       <button onClick={() => setModal("")}>Cancel</button>
-//     </div>
-//   );
-// }
-/* ------------------- Ends ------------------- */
-
-/* ==============================================
-          Leftover Detail Component
-=============================================== */
 function LeftoverDetail(props) {
   const [modal, setModal] = useState();
   const [leftover, setLeftover] = useState();
--- src/components/leftovers/Leftovers.jsx
+++ src/components/leftovers/Leftovers.jsx
@@ -6,24 +6,40 @@ import Leftover from "./Leftover";
 function Leftovers() {
   const [leftovers, setLeftovers] = useState([]);
   const { state, api_url } = useContext(LeftoverContext);
+  const [myFridgeIsClicked, setMyFridgeIsClicked] = useState(false);
 
   function getLeftovers() {
-    const offset = new Date("1969-07-20T20:17:40.00Z").getTimezoneOffset() * 1000 * 60
-    console.log("local time offset:", offset/1000/60/60, "hours")
+    const offset =
+      new Date("1969-07-20T20:17:40.00Z").getTimezoneOffset() * 1000 * 60;
+    console.log("local time offset:", offset / 1000 / 60 / 60, "hours");
     axios
       .get(`${api_url}/leftovers/`)
       .then((res) => {
-        const filteredResponse = res.data.filter((i) => {
-          const timeLeft = new Date(i.expiration) - new Date().getTime() + offset;
+        // Filters expired leftovers out
+        const availableLeftovers = res.data.filter((i) => {
+          const timeLeft =
+            new Date(i.expiration) - new Date().getTime() + offset;
           return timeLeft > 0;
         });
-        const sortedResponse = filteredResponse.sort((a, b) => {
+
+        // Sorts by earliest to furthest expiration date
+        const sortedLeftovers = availableLeftovers.sort((a, b) => {
           return new Date(a.expiration) - new Date(b.expiration);
         });
-        console.log("sorted:=>", sortedResponse);
-        setLeftovers(sortedResponse);
+        console.log("sorted:=>", sortedLeftovers);
+
+        // Filters to the user's leftovers if "My Fridge" is clicked
+        // otherwise filters to public leftovers
+        const personalLeftovers = sortedLeftovers.filter(
+          (i) => i.owner === state.username
+        );
+        const publicLeftovers = sortedLeftovers.filter((i) => i.is_public);
+
+        myFridgeIsClicked
+          ? setLeftovers(personalLeftovers)
+          : setLeftovers(publicLeftovers);
       })
-      .catch(console.error);  
+      .catch(console.error);
   }
 
   useEffect(() => {
