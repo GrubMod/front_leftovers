c0e95d6aefd74eeac33fb2b53bf8be485d093071
rsaleh <raihan.saleh@gmail.com>
2021-06-13 01:34:28 -0400

    leftovers filtered and sorted

--- src/components/leftovers/Leftovers.jsx
+++ src/components/leftovers/Leftovers.jsx
@@ -2,22 +2,30 @@ import React, { useEffect, useState, useContext } from "react";
 import axios from "axios";
 import { LeftoverContext } from "../../LeftoverContext";
 import Leftover from "./Leftover";
-import LeftoverForm from "./LeftoverForm";
-import FoodImageForm from "./FoodImageForm";
 
-function Leftovers(props) {
+function Leftovers() {
   const [leftovers, setLeftovers] = useState([]);
   const { state, api_url } = useContext(LeftoverContext);
 
   function getLeftovers() {
+    const offset = new Date("1969-07-20T20:17:40.00Z").getTimezoneOffset() * 1000 * 60
+    console.log("local time offset:", offset/1000/60/60, "hours")
     axios
       .get(`${api_url}/leftovers/`)
       .then((res) => {
-        setLeftovers(res.data);
-        console.log(res.data);
+        const filteredResponse = res.data.filter((i) => {
+          const timeLeft = new Date(i.expiration) - new Date().getTime() + offset;
+          return timeLeft > 0;
+        });
+        const sortedResponse = filteredResponse.sort((a, b) => {
+          return new Date(a.expiration) - new Date(b.expiration);
+        });
+        console.log("sorted:=>", sortedResponse);
+        setLeftovers(sortedResponse);
       })
-      .catch(console.error);
+      .catch(console.error);  
   }
+
   useEffect(() => {
     getLeftovers();
   }, [api_url]);
